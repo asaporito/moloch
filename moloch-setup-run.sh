6eed91a3807564649118d166c10ae06943754a45
asaporito <andrewcsaporito@gmail.com>
2017-07-25 11:54:39 -0400

    Update moloch-setup-run.sh

--- moloch-setup-run.sh
+++ moloch-setup-run.sh
@@ -5,3 +5,5 @@ systemctl status elasticsearch.service
 /data/moloch/bin/moloch_add_user.sh admin admin admin --admin
 systemctl start molochcapture.service
 systemctl start molochviewer.service
+systemctl status molochcapture.service
+systemctl status molochviewer.service
