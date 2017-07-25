2a677c42e500b6fc3a64ec07562079c6c0920af2
asaporito <andrewcsaporito@gmail.com>
2017-07-25 11:58:59 -0400

    Update moloch-setup-run.sh

--- moloch-setup-run.sh
+++ moloch-setup-run.sh
@@ -1,3 +1,4 @@
+echo running script to setup moloch and after start moloch.
 systemctl start elasticsearch.service
 systemctl enable elasticsearch.service
 systemctl status elasticsearch.service
@@ -7,3 +8,4 @@ systemctl start molochcapture.service
 systemctl start molochviewer.service
 systemctl status molochcapture.service
 systemctl status molochviewer.service
+echo setup is now complete please go to http://localhost:8005 and the username and password is admin.
