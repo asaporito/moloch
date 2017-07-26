645e34e975db684cb51fbd7cda0c6a73b883f395
asaporito <andrewcsaporito@gmail.com>
2017-07-26 08:12:21 -0400

    Update docker-network-setup.sh

--- docker-network-setup.sh
+++ docker-network-setup.sh
@@ -2,5 +2,7 @@
 sudo docker pull asaporito/moloch
 sudo docker run --rm --privileged -v /:/host asaporito/moloch setup
 sudo docker run -d --net="host" --name moloch --security-opt seccomp=unconfined --tmpfs /run -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /home/pcasStorage:/data/moloch/raw -t asaporito/moloch
+echo '*'                -      nofile          128000 >> /etc/security/limits.conf
+echo '*'                -      memlock         unlimited >> /etc/security/limits.conf
 sudo docker exec -it moloch /bin/bash
-./data/moloch/bin/Configure
+
