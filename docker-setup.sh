00be064ef23e299090633da806a752bc41148e89
asaporito <andrewcsaporito@gmail.com>
2017-07-27 08:31:42 -0400

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -4,11 +4,10 @@ sudo docker run -d --net="host" --name moloch --security-opt seccomp=unconfined 
 sudo echo '*'                -      nofile          128000 >> /etc/security/limits.conf
 sudo echo '*'                -      memlock         unlimited >> /etc/security/limits.conf
  if [ -z "$MOLOCH_INTERFACE" ]; then 
-   echo -n "Found interfaces: "
-   /sbin/ifconfig | grep "^[a-z]" | cut -d: -f1 | cut -d" " -f1 | paste -s -d
-   echo -n "Semicolon ';' seperated list of interfaces to monitor one more time to disable some network card features: "
+   echo -n "Found interfaces: " /sbin/ifconfig | grep "^[a-z]" | cut -d: -f1 | cut -d" " -f1 | paste -s -d";"
+   echo -n "Semicolon ';' seperated list of interfaces that you will be monitoring with Moloch "
    read -r MOLOCH_INTERFACE
-   ethtool -K $MOLOCH_INTERFACE tx off sg off gro off gso off lro off tso off
+   sudo ethtool -K $MOLOCH_INTERFACE tx off sg off gro off gso off lro off tso off
    echo $MOLOCH_INTERFACE
  fi
 sudo docker exec -it moloch /bin/bash
